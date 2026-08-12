c9316b07b63473cbfc26c071b3e6516c68ef66aa
Ankur Singh <ankursingh9998@gmail.com>
2026-08-12 05:45:03 +0530

    fix: configure production cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: env.CORS_ORIGIN === '*' ? true : env.CORS_ORIGIN.split(','),
+    origin: env.CORS_ORIGIN.split(',').map((origin) => origin.trim()),
     credentials: true,
   })
 );
